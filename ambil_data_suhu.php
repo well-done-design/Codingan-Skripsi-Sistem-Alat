bfd080ce873959a233b5c75908dc625ca2880283
Nur Amri Wildani <wildaaan.w12@gmail.com>
2025-08-14 14:18:05 +0700

    Create ambil_data_suhu.php

---
+++ ambil_data_suhu.php
@@ -0,0 +1,14 @@
+<?php
+    $konek = mysqli_connect("localhost", "xigagiwx_magrowkit", "Natuna.1234567890", "xigagiwx_db_multisensor");
+    $sql = mysqli_query($konek, "select * from tb_lalat order by id desc");
+    $data = mysqli_fetch_array($sql);
+    $temperature = $data['temperature'];
+    if($temperature == "") $temperature = 0;
+    echo $temperature;
+
+
+
+
+
+
+?>
